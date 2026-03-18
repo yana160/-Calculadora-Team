9e864b9725a8630a4ffb988f8bef3e6ea16edb93
Diurno <Diurno@DAM116>
2026-03-18 10:12:15 +0100

    cambios division

--- Proyecto/src/Entornos/Calculadora.java
+++ Proyecto/src/Entornos/Calculadora.java
@@ -4,6 +4,7 @@ public class Calculadora {
 
 		 public double sumar(double a, double b) { return a + b; }
 		 public double restar(double a, double b) { return a - b; }
+		 public double dividir(double a, double b) { return a / b; }
 		 public static void main(String[] args) {
 		 Calculadora calc = new Calculadora();
 		 System.out.println("--- INICIO DE LA CALCULADORA ---");
